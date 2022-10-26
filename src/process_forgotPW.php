c94afe911269c082a56c5aeb54465b2383a62d6a
Kou <kou.yang2015@gmail.com>
2022-10-26 16:59:07 -0500

    Created page templates for all sites.

---
+++ src/process_forgotPW.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Illumination Music - Forgot Password Results</title>
+  </head>
+  <body>
+    <h1 align="center">Forgot Password Results</h1>
+    <p>Page under construction. Look at mockup for template.</p>
+  </body>
+</html>
