1e06b14e003ca6a33dbfa0bac963c22cf92b7746
Kou Yang <80478758+kouYang2015@users.noreply.github.com>
2022-10-26 17:00:29 -0500

    Created page templates for all sites. (#2)
    Just made page templates.

---
+++ src/homePage.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Illumination Music - Home</title>
+  </head>
+  <body>
+    <h1 align="center">Home</h1>
+    <p>Page under construction. Look at mockup for template.</p>
+  </body>
+</html>
---
+++ src/process_signup.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Illumination Music - Signup Results</title>
+  </head>
+  <body>
+    <h1 align="center">Signup Results</h1>
+    <p>Page under construction. Forgot password page. Look at mockup for template.</p>
+  </body>
+</html>
